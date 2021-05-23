42d2b21a81dbba1e907a85b0613a622ecc563726
Drznedaj <cvajblumen@gmail.com>
2021-05-23 22:29:57 +0200

    Protect editor only pages

--- linkgraph/linkgraph_app/views.py
+++ linkgraph/linkgraph_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.urls import reverse
 from django.utils import timezone
 from django.db.models import Q
@@ -56,6 +57,27 @@ def create(request):
     return render(request, "app/create.html", {"form": form})
 
 
+def check_user_is_editor(user):
+    return user.is_editor
+
+
+def check_user(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    """
+    Decorator for views that checks that the user is logged in, redirecting
+    to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        check_user_is_editor,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+
+@login_required
+@check_user(login_url="/")
 def article_approval(request):
 
     articles = Article.objects.filter(status=ArticleStatusTypes.NOT_REVIEWED)
@@ -63,6 +85,8 @@ def article_approval(request):
     return render(request, "app/article_approval.html", {"articles": articles})
 
 
+@login_required
+@check_user(login_url="/")
 def approve(request, article_id):
     article = Article.objects.get(id=article_id)
 
@@ -75,6 +99,8 @@ def approve(request, article_id):
     return render(request, "app/article_approval.html", {"articles": articles})
 
 
+@login_required
+@check_user(login_url="/")
 def reject(request, article_id):
     article = Article.objects.get(id=article_id)
 
@@ -87,6 +113,8 @@ def reject(request, article_id):
     return render(request, "app/article_approval.html", {"articles": articles})
 
 
+@login_required
+@check_user(login_url="/")
 def articles_edited(request):
     articles = Article.objects.filter(Q(edited_by=request.user) & ~Q(status=ArticleStatusTypes.NOT_REVIEWED))
     return render(request, "app/articles_edited.html", {"articles": articles})
